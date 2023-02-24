8e84e58aeef269468da5d7772a2b7b1dde319e44
TrueMishka <Mixxxa32123@gmail.com>
2023-02-24 22:10:10 +0300

    skills icons

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,6 +4,11 @@ import commonStyle from '../../common/style/CommonStyle.module.css'
 import {PageTitle} from "../pageTitle/PageTitle";
 import {Element} from "react-scroll";
 import {Skill} from "./skill/Skill";
+import { FaHtml5 } from 'react-icons/fa'
+import { TbNumber1 } from 'react-icons/tb'
+import { SiCss3, SiTypescript, SiReact, SiRedux } from 'react-icons/si'
+
+
 
 export const Skills = () => {
     return (
@@ -16,24 +21,39 @@ export const Skills = () => {
                             <Skill
                                 title={'HTML'}
                                 description={'HTML (HyperText Markup Language) is the code that is used to structure a web page and its content.'}
-                            />
+                            >
+                                <FaHtml5 size={50}/>
+                            </Skill>
                             <Skill
                                 title={'CSS'}
                                 description={'CSS is the language we use to style an HTML document. CSS describes how HTML elements should be displayed.'}
-                            />
+                            >
+                                <SiCss3 size={45}/>
+                            </Skill>
                             <Skill
                                 title={'TS'}
                                 description={'TypeScript is a strongly typed programming language that builds on JavaScript.'}
-                            />
+                            >
+                                <SiTypescript size={45}/>
+                            </Skill>
                             <Skill
                                 title={'React'}
                                 description={'A JavaScript library for building user interfaces.'}
-                            />
+                            >
+                                <SiReact size={45}/>
+                            </Skill>
                             <Skill
                                 title={'Redux'}
                                 description={'Redux is a predictable state container for JavaScript apps. It helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test.'}
-                            />
-                            <Skill title={'1C'} description={'uwu'}/>
+                            >
+                                <SiRedux size={45}/>
+                            </Skill>
+                            <Skill
+                                title={'1C'}
+                                description={'uwu'}
+                            >
+                                <TbNumber1 size={45}/>
+                            </Skill>
                         </div>
                     </div>
                 </div>
--- src/components/skills/skill/Skill.tsx
+++ src/components/skills/skill/Skill.tsx
@@ -4,12 +4,17 @@ import classes from "./Skill.module.css";
 type PropsType = {
     title: string
     description: string
+    children?: JSX.Element
 }
 
-export const Skill:React.FC<PropsType> = ({title, description}) => {
+export const Skill:React.FC<PropsType> = ({title, description, children}) => {
     return (
         <div className={classes.skill}>
-            <div className={classes.icon}></div>
+            <div className={classes.iconContainer}>
+                <div className={classes.icon}>
+                    {children}
+                </div>
+            </div>
             <div className={classes.information}>
                 <h3 className={classes.title}>{title}</h3>
                 <p className={classes.description}>{description}</p>
