9e96177849aa4fe1a0f12595131bd660b0c5361d
TrueMishka <Mixxxa32123@gmail.com>
2023-03-26 16:41:39 +0300

    hamburger menu

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./Sidebar.module.scss";
 import {Link} from "react-scroll";
+import {RxHamburgerMenu} from 'react-icons/rx'
+import {TbBrandGithub, TbBrandTelegram} from 'react-icons/tb'
+import {CiLinkedin} from 'react-icons/ci'
 
 export const Sidebar = () => {
+
+    const [displayHamburgerMenu, setDisplayHamburgerMenu] = useState(false)
+    const hamburgerMenuHandler = () => {
+        setDisplayHamburgerMenu(!displayHamburgerMenu)
+    }
+
+    const closeHamburgerMenu = () => {
+        setDisplayHamburgerMenu(false)
+    }
+
+    const body = document.querySelector('body');
+    if (body) {
+        body.style.overflow = displayHamburgerMenu ? 'hidden' : 'unset'
+    }
+
+    const navMenu = <ul>
+        <li><Link onClick={closeHamburgerMenu} activeClass={classes.active} to={'Home'} spy={true} smooth={true}>Home</Link></li>
+        <li><Link onClick={closeHamburgerMenu} activeClass={classes.active} to={'AboutMe'} spy={true} smooth={true}>About Me</Link></li>
+        <li><Link onClick={closeHamburgerMenu} activeClass={classes.active} to={'Skills'} spy={true} smooth={true}>What I Know</Link></li>
+        <li><Link onClick={closeHamburgerMenu} activeClass={classes.active} to={'Portfolio'} spy={true} smooth={true}>Portfolio</Link></li>
+        <li><Link onClick={closeHamburgerMenu} activeClass={classes.active} to={'Contact'} spy={true} smooth={true}>Contact</Link></li>
+    </ul>
+
     return (
         <div className={classes.wrapper}>
             <div className={classes.container}>
@@ -11,20 +37,19 @@ export const Sidebar = () => {
                     <h1 className={classes.photoText}>Mishka</h1>
                 </div>
                 <div className={classes.navContainer}>
-                    <ul>
-                        <li><Link activeClass={classes.active} to={'Home'} spy={true} smooth={true}>Home</Link></li>
-                        <li><Link activeClass={classes.active} to={'AboutMe'} spy={true} smooth={true}>About Me</Link></li>
-                        <li><Link activeClass={classes.active} to={'Skills'} spy={true} smooth={true}>What I Know</Link></li>
-                        <li><Link activeClass={classes.active} to={'Portfolio'} spy={true} smooth={true}>Portfolio</Link></li>
-                        <li><Link activeClass={classes.active} to={'Contact'} spy={true} smooth={true}>Contact</Link></li>
-                    </ul>
+                    {navMenu}
                 </div>
                 <div className={classes.footerContainer}>
-                    Some icon (linkedIn Telegram etc.)
+                    <TbBrandGithub className={classes.footerIcon} size={25}/>
+                    <TbBrandTelegram className={classes.footerIcon} size={25}/>
+                    <CiLinkedin className={classes.footerIcon} size={25}/>
                 </div>
-                {/*<div className={classes.btmHamburger}>
-                    <button>btn</button>
-                </div>*/}
+                <div className={classes.btmHamburger}>
+                    <RxHamburgerMenu size={30} onClick={hamburgerMenuHandler}/>
+                    {displayHamburgerMenu && <div className={classes.hamburgerMenu}>
+                        {navMenu}
+                    </div>}
+                </div>
             </div>
         </div>
     );
